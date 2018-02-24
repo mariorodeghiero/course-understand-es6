64cfe7122db3d99febffd1439bd1667b1dadb8f9
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-02-24 14:15:51 -0300

    add foreach and map

---
+++ 01-methods/01-foreach.js
@@ -0,0 +1,14 @@
+var arr = ["mario", "bento", "tofu"];
+
+arr.forEach(function(name) {
+  console.log(name);
+});
+
+let numero = [4, 3, 5, 7];
+let dobro = [];
+
+numero.forEach(function(num) {
+  dobro.push(num * 2);
+});
+
+console.log(dobro);
---
+++ 01-methods/02-map.js
@@ -0,0 +1,9 @@
+let numbers = [2, 3, 4, 5];
+
+let double = numbers.map(function(num) {
+  return num * 2;
+});
+
+console.log("------------------------------------");
+console.log(double);
+console.log("------------------------------------");
