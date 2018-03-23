6cc47cec5ad445bcfcdffc0cc60e13b02854b49c
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-03-23 00:06:42 -0300

    default values

---
+++ 06-values-default/01-value-undefined.js
@@ -0,0 +1,7 @@
+function showFullName(par1, par2, par3 = "Amaral") {
+  console.log("------------------------------------");
+  console.log(`${par1} ${par2} ${par3}`);
+  console.log("------------------------------------");
+}
+
+showFullName("Mário", "Rodeghiero");
